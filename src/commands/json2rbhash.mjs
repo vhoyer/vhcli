1961ec54bab5a89ddd884cc71c013e573a1badd0
Vinícius Hoyer <contact@vhoyer.dev>
2021-02-15 14:32:21 -0300

    :construction: Sei lá, bro

--- src/commands/json2rbhash.mjs
+++ src/commands/json2rbhash.mjs
@@ -2,12 +2,10 @@ import camelcase from 'camelcase-keys'
 import { stringify } from 'javascript-stringify'
 import clipboardy from 'clipboardy';
 
-const doIt = (str) => JSON.stringify(camelcase(str, { deep: true }), null, 2);
-
-
+const doIt = (str) => stringify(camelcase(str, { deep: true }), null, 2);
 
 process.stdin.on('data', (data) => {
-  eval(`global.suckIt = ${clipboardy.readSync()}`)
+  eval(`global.suckIt = ${data.toString()}`)
 
   clipboardy.writeSync(doIt(global.suckIt));
 })
